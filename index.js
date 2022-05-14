3051c6660eaffbadab73c5dda3178a12c80e2023
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-05-14 08:34:19 +0600

    bookin post api created with query paramter

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// pass: IqQgrBVlTcnyBYis
-// user: Rasel
-
 
 // const { MongoClient, ServerApiVersion } = require('mongodb');
 // const uri = "mongodb+srv://Rasel:<password>@cluster0.ubb0j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
@@ -14,6 +11,8 @@
 const express = require('express')
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -21,6 +20,62 @@ const port = process.env.PORT || 5000;
 // middleware  
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ubb0j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+  try {
+    await client.connect();
+    const serviceCollection = client.db("doctors_portal").collection("services");
+    const bookingCollection = client.db("doctors_portal").collection("bookings");
+
+
+    app.get("/service", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    app.get("/available", async (req, res) => {
+      
+    })
+
+    // post patient info 
+    // app.post('/patients', async (req, res) => {
+    //   const newPatient = req.body;
+    //   const result = await patientCollection.insertOne(newPatient);
+    //   res.send(result);
+    // });
+
+    /**
+        * API naming onvenstion
+        * app.get("/booking") get all booking or more than one by filter
+        * app.get("/booking/:id")  get a specific booking
+        * app.post("/booking") add a new booking
+        * app.patch("/booking/:id") /update
+        * app.delete("/booking/:id") /delete
+        */
+
+    app.post('/booking', async (req, res) => {
+      const booking = req.body;
+      const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient };
+      const exists = await bookingCollection.findOne(query);
+      if (exists) {
+        return res.send({ success: false, booking: exists });
+      }
+      const result = await bookingCollection.insertOne(booking);
+      return res.send({ success: true, result });
+    })
+
+  }
+  finally {
+
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('Hello From doctor Uncle')
